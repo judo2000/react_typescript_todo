a51a6baddf64de733de9b292c542b133f337254b
Stephen Moore <judo2000@comcast.net>
2022-05-18 16:48:57 -0600

    Initial commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+const InputField = () => {
+  return (
+    <div>
+      <form className='input'>
+        <input type='input' className='input__box' placeholder='Enter a task' />
+        <button type='submit' className='input__submit'>
+          Go
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputField;
