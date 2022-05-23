f45ef6f86d1b458cc7ffb31a8a36b3c9bf5be922
Stephen Moore <judo2000@comcast.net>
2022-05-23 17:51:19 -0600

    Complete adding todos to todos array

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
+import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
     <div>
-      <form className='input'>
-        <input type='input' className='input__box' placeholder='Enter a task' />
+      <form className='input' onSubmit={handleAdd}>
+        <input
+          type='input'
+          className='input__box'
+          placeholder='Enter a task'
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
         <button type='submit' className='input_submit'>
           Go
         </button>
